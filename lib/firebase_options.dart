5c8469448c73a598d133bb3ecf49c95a68c52ab8
Killian El Attar <119617920+Tatar73@users.noreply.github.com>
2026-04-16 23:02:02 +0200

    refactor: migrate Firebase iOS configuration to use environment variables via flutter_dotenv

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,12 +53,12 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCbvq8HnqyvI2rUYXND08t7M71S8ZjuK20',
-    appId: '1:461785581723:ios:98a559ca59b3b264f99e3e',
-    messagingSenderId: '461785581723',
-    projectId: 'panar-app',
-    storageBucket: 'panar-app.firebasestorage.app',
-    iosBundleId: 'com.brazil.Panar.health',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+    iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? '',
   );
 }
